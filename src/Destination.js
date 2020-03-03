a70263fbf45b42f03118a85adb91cc30157e930b
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-03-02 19:11:42 -0700

    Update classes

--- src/Destination.js
+++ src/Destination.js
@@ -1,11 +1,11 @@
 class Destination {
   constructor({id, destination, estimatedLodgingCostPerDay, estimatedFlightCostPerPerson, image, alt}) {
-    this.id = id;
-    this.destination = destination;
-    this.city = destination.split(', ')[0];
-    this.country = destination.split(', ')[1];
-    this.estimatedLodgingCostPerDay = estimatedLodgingCostPerDay;
-    this.estimatedFlightCostPerPerson = estimatedFlightCostPerPerson;
+    this.id = id || 0;
+    this.destination = destination || 'Nowhere, Nowhere';
+    this.city = this.destination.split(', ')[0];
+    this.country = this.destination.split(', ')[1];
+    this.estimatedLodgingCostPerDay = estimatedLodgingCostPerDay || 0;
+    this.estimatedFlightCostPerPerson = estimatedFlightCostPerPerson || 0;
     this.image = image;
     this.alt = alt;
   }
--- src/Traveler.js
+++ src/Traveler.js
@@ -1,8 +1,35 @@
 class Traveler {
-  constructor({id, name, travelerType}) {
+  constructor({id, name, travelerType}, trips) {
     this.id = id;
     this.name = name;
     this.type = travelerType;
+    this.trips = this.sortTrips(trips);
+  }
+
+  sortTrips(trips) {
+    return trips.sort((tripA, tripB) => {
+      let dateA = new Date(tripA.date);
+      let dateB = new Date(tripB.date);
+      return dateB - dateA;
+    });
+  }
+
+  calculateYearlyCost() {
+    let thisYearsTrips = this.trips.filter(trip => {
+      let thisYear = new Date(Date.now()).getFullYear();
+      let tripYear = Number(trip.date.slice(0, 4));
+      return thisYear === tripYear;
+    })
+    let yearlyCost = thisYearsTrips.reduce((totalCost, trip) => {
+      let cost = trip.estimateCost();
+      let parsedCost = Number(cost.replace(/,/g, ''));
+      return totalCost + parsedCost;
+    }, 0);
+    return (yearlyCost).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+  }
+
+  addTrip(trip) {
+    this.trips = this.sortTrips(this.trips.concat(trip));
   }
 }
 
